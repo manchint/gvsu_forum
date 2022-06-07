974b9aea7cd1d9ee3c12c5b38321da1467e3f955
manchint <manchint@mail.gvsu.edu>
2022-06-07 13:09:29 -0400

    folder structure initialization

---
+++ components/PostComponent.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const PostComponent = () => {
+    return (
+        <></>
+    )
+}
+
+export default PostComponent;
