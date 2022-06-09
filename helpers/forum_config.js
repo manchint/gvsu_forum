afc1035dfbc38134e262fb61e2b81b3e8dd63fbd
thakkilm <105686015+thakkilm@users.noreply.github.com>
2022-06-09 00:46:39 -0400

    DrawNavigation
    -> Have tried implementing Draw Navigation functionality but ran into few issues.
    
    -> Have added the firebaseconfig method with valid Api key

--- helpers/forum_config.js
+++ helpers/forum_config.js
@@ -1,16 +1,34 @@
-import { getDatabase, onValue, push, ref, remove, set } from "firebase/database";
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  remove,
+  set,
+} from "firebase/database";
 import { initializeApp } from "firebase/app";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCFQeec4C_5v_HuCIRjCACV95__H9I1nRU",
+//     authDomain: "geocalculator-98c0b.firebaseapp.com",
+//     projectId: "geocalculator-98c0b",
+//     storageBucket: "geocalculator-98c0b.appspot.com",
+//     messagingSenderId: "1072563158551",
+//     appId: "1:1072563158551:web:ceae2d595b8592ec4d2652",
+//     measurementId: "G-DYS7TTTZCL"
+// };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCFQeec4C_5v_HuCIRjCACV95__H9I1nRU",
-    authDomain: "geocalculator-98c0b.firebaseapp.com",
-    projectId: "geocalculator-98c0b",
-    storageBucket: "geocalculator-98c0b.appspot.com",
-    messagingSenderId: "1072563158551",
-    appId: "1:1072563158551:web:ceae2d595b8592ec4d2652",
-    measurementId: "G-DYS7TTTZCL"
+  apiKey: "AIzaSyB9jHrn_3vLNAJ9wq6zbGZblmlS7CSWSZg",
+  authDomain: "gvsuforum-baa4f.firebaseapp.com",
+  databaseURL: "https://gvsuforum-baa4f-default-rtdb.firebaseio.com",
+  projectId: "gvsuforum-baa4f",
+  storageBucket: "gvsuforum-baa4f.appspot.com",
+  messagingSenderId: "317521063061",
+  appId: "1:317521063061:web:47b80322e453e8a2bb7f9c",
+  measurementId: "G-M8ZB1NERR9",
 };
+
 export function initGvsuForumDB() {
   initializeApp(firebaseConfig);
-};
+}
---
+++ screens/DrawNavigation.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Button, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigation() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
