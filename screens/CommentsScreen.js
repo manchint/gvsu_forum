e0f14b30863dc9b1871d56515ee15d363b882a24
manchint <manchint@mail.gvsu.edu>
2022-06-17 14:27:46 -0400

    comments screen

--- screens/CommentsScreen.js
+++ screens/CommentsScreen.js
@@ -1,33 +1,24 @@
 import React, { useState } from "react";
 import {
-  StyleSheet,
   Text,
   View,
-  TouchableOpacity,
   FlatList,
   Linking,
 } from "react-native";
 import { updatePost } from "../helpers/forum_posts";
 import { getData } from "../helpers/storage_init";
 import { Button, Input, Card } from "react-native-elements";
-const CommentsScreen = ({ route, navigation }) => {
+const CommentsScreen = ({ route }) => {
   const [loginData, setLoginData] = useState();
+  const [comments, setComments] = useState([])
   getData(setLoginData);
-  console.log("jgfefwehfhwefhuwejdkjwefhjcbweh", route.params);
   const [comment, setComment] = useState("");
   const renderEachComment = (data) => {
-    console.log("data", data);
+      console.log("dhsahf", data)
     return (
       <View>
-        <View>
-          <Text item={data.item} />
-        </View>
-        <View>
-          {/* <Input
-                    value={comment}
-                    onChangeText={(val) => setComment(val)}></Input>
-                    <Button>comment</Button> */}
-        </View>
+          <Text item={data.item.comment} />
+          <Text item={data.item.name} />
       </View>
     );
   };
@@ -35,21 +26,16 @@ const CommentsScreen = ({ route, navigation }) => {
     let comments = route.params.data.comments;
     if (comments === 0) {
       comments = [{ name: loginData.email, comment: comment }];
-      //updatePost({ ...props.item, likes: ['sdcsdc']});
     } else {
-      comments.push([{ name: loginData.email, comment: comment }]);
-      // if (checkCurrentUserLike()) {
-      //     likes = postData.likes.filter(item => item !== 'fdvcdfvx')
-      // }
-      //updatePost({ ...props.item, likes: ['sdcsdc']});
+      comments.push({ name: loginData.email, comment: comment });
     }
     updatePost({ ...route.params.data, comments: comments });
   };
   return (
     <View>
-      <FlatList data={route.data} renderItem={renderEachComment} />
       <Input value={comment} onChangeText={(val) => setComment(val)}></Input>
       <Button title={"Comment"} onPress={updateComment}></Button>
+      <FlatList delayUpdate={true} data={route.params.data.comments} renderItem={renderEachComment} />
     </View>
   );
 };
